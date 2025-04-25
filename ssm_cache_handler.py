2bbe24ed1e7eec2d42439ca02ad669cf62ccc442
Kazalo11 <kazoshodi@gmail.com>
2025-04-25 14:34:53 +0100

    add logs

--- ssm_cache_handler.py
+++ ssm_cache_handler.py
@@ -6,10 +6,12 @@ import spotipy
 
 class SSMCacheHandler(spotipy.CacheHandler):
 	def __init__(self):
+		print("Initalising cache handler")
 		self.client = boto3.client("ssm")
 		self.parameter_name = "auto-update-spotify-cache-token"
 	
 	def get_cached_token(self):
+		print("Trying to get cached token from parameter store")
 		response = self.client.get_parameter(
 			Name=self.parameter_name,
 			WithDecryption=False
@@ -17,6 +19,7 @@ class SSMCacheHandler(spotipy.CacheHandler):
 		return json.loads(response['Parameter']['Value'])
 
 	def save_token_to_cache(self, token_info):
+		print("Trying to save cached token to parameter store")
 		self.client.put_parameter(
 			Name=self.parameter_name,
 			Value=json.dumps(token_info, ensure_ascii=False),
