7529eab3d0de180c39c74f9087baf09e6ad4aa99
Kazalo11 <kazoshodi@gmail.com>
2025-07-05 18:08:54 +0100

    add in logging + raising errors

--- dynamo_db_handler.py
+++ dynamo_db_handler.py
@@ -1,5 +1,8 @@
 import boto3
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
 
 class DynamoDBHandler:
 
@@ -7,5 +10,9 @@ class DynamoDBHandler:
         self.table = boto3.resource('dynamodb').Table(table_name)
 
     def check_if_song_exists_in_db(self, song_id: str) -> bool:
-        response = self.table.get_item(Key={'spotify_id': song_id})
-        return 'Item' in response
+        try:
+            response = self.table.get_item(Key={'spotify_id': song_id})
+            return 'Item' in response
+        except Exception as e:
+            logging.error(f"Error checking if song exists in DB: {e}")
+            raise
